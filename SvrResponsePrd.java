f3ee500a241d7e0e42a7ab833ba8644d64f01328
hoanghungdz <hoanghung200603@gmail.com>
2024-04-05 00:14:00 +0700

    lab7

---
+++ SvrResponsePrd.java
@@ -0,0 +1,14 @@
+package com.hungnq123.myapplication12.retrofit1;
+
+public class SvrResponsePrd {//GET
+    private Prd products;
+    private String message;
+
+    public Prd getProducts() {
+        return products;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ SvrResponseProd.java
@@ -0,0 +1,14 @@
+package com.hungnq123.myapplication12.retrofit1;
+
+public class SvrResponseProd {//GET
+    private Prod[] products;
+    private String message;
+
+    public Prod[] getProducts() {
+        return products;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
